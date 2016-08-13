e85faacb962e7575c917b7bec6ef496ea954ac35
tobbenb <torbjornbrekke@gmail.com>
2016-08-13 18:50:03 +0200

    First commit

---
+++ docker-tvheadend-unstable/root/etc/cont-init.d/30-startup.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/with-contenv bash
+
+#!/bin/bash
+echo "Running startup script...."
+
+
+#Check if nonexistent folder exists and create if needed
+#if [ ! -d /nonexistent ]; then
+#	echo "Creating directory /nonexistent."
+#	mkdir -p /nonexistent/
+#else 
+#	echo "The directory /nonexisten already exist."
+#fi
+
+
+#Check if xmltv folder exists
+#if [ ! -d /config/.xmltv ]; then
+#	echo "Creating directory /config/.xmltv."
+#	mkdir -p /config/.xmltv
+#	ln -s  /config/.xmltv /nonexistent/
+#else 
+#	echo "The directory /config/.xmltv already exist."
+#fi
+
+#Check if link exists
+#if [ ! -h /config/.xmltv ]; then
+#	echo "Creating symlink to directory /nonexistent."
+#	ln -s  /config/.xmltv /nonexistent/
+#else
+#	echo "Symlink to /nonexistent already exist."
+#fi
+
+#Add default recording path if no config exists
+if [ ! -d /config/.hts/dvr/config ]; then
+	echo "Creating default DVR config..."
+	mkdir -p -u 755 /config/.hts/tvheadend/dvr/config
+	cp /defaults/7a5edfbe189851e5b1d1df19c93962f0 /config/.hts/tvheadend/dvr/config/7a5edfbe189851e5b1d1df19c93962f0
+else
+	echo "Config already exists. Skipping default DVR config setup."
+fi
+
+#Comskip.ini check.
+if [ ! -f /config/comskip/comskip.ini ]; then
+	echo "Copying deafult comskip.ini to /config/comskip/."
+	mkdir -p -u 755 /config/comskip
+	cp /defaults/comskip.ini.org /config/comskip/comskip.ini
+else
+	echo "/etc/comskip.ini already exists."
+fi
+
+#echo "Changing owner and permissions for /config/"
+#chown -R abc:abc /config/
+#chmod -R g+rw /config/
+
+
+ 
